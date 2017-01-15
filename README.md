6ba61110d4102f682f36e9cb73f3e4f870fba70b
Hans Bakker <hansmbakker+github@gmail.com>
2017-01-15 22:30:17 +0100

    Update readme, attribute copyright Update nuget

--- README.md
+++ README.md
@@ -1,59 +1,10 @@
-Nmea Parser
+Grbl connector
 =========
 
-Library for reading and parsing NMEA data message streams.
-It makes it easy to connect and listen for NMEA messages coming from various devices in Windows Store, Windows Phone, Windows Desktop/.NET and Windows Universal apps as well as Xamarin for iOS and Android.
+Library for connecting to Grbl and communicating with it.
 
-The following inputs are supported:
-- System.IO.Stream (all platforms)
-- Emulation from NMEA log file (all platforms)
-- Bluetooth: Windows Store, Windows Phone, Windows Universal. Desktop is supported using the bluetooth device via the SerialPortDevice.
-- Serial Device: Windows Desktop and Windows Universal.
+Supports Grbl 1.1; it is built based on the documentation at https://github.com/gnea/grbl/blob/master/doc/markdown/interface.md.
 
-
-Currently supported NMEA messages:
-- Generic GPS NMEA (GPRMC, GPGGA, GPGLL, GPGSA, GPGSCV, GPRMB, GPRMC, GPBOD, GPRTE, GPGST)
-- Garmin GPS NMEA (PGRME, PGRMZ)
-- Trimble Laser Range Finder (PTNLA, PTNLB)
-- TruePulse Laser Range Finder (PLTIT)
-
-The API is easily extensible with more NMEA messages. Simply create a new class inheriting from "NmeaMessage" and use the NmeaMessageType Attribute to tag it with the NMEA Message Token it supports.
-
-Example:
-```csharp
-[NmeaMessageType("GPRMC")]
-public class Gprmc : NmeaMessage
-{
-	protected override void LoadMessage(string[] message)
-	{
-		//TODO: Process message parts
-	}
-}
-```
-
-If you add new messages, please fork, provide a simple unit test for the message and submit a pull request.
-
-
-### NuGet
-You can get the library via [NuGet](http://www.nuget.org) if you have the extension installed for Visual Studio or via the PowerShell package manager.  This control is published via NuGet at [SharpGIS.NmeaParser](https://nuget.org/packages/SharpGIS.NmeaParser).
-
-<table border="4px">
-<tr><td>
-            <code>PM&gt; Install-Package SharpGIS.NmeaParser</code>
-</td></tr></table>
-
-Usage
-=====================
-
-Please see the [WIKI](http://www.github.com/dotMorten/NmeaParser/wiki) how to use it on the various platforms
-
-Screenshots
-=====================
-Screenshots from Desktop sample app:
-
-![sampleapp2](https://cloud.githubusercontent.com/assets/1378165/5062460/10cc3064-6d77-11e4-8365-1e9c7c346afc.png)
-![sampleapp3](https://cloud.githubusercontent.com/assets/1378165/5062461/123adfc2-6d77-11e4-8573-1fe95fa0325f.png)
-
-Map view using the [ArcGIS Runtime](http://developer.arcgis.com/net) (see [separate branch](https://github.com/dotMorten/NmeaParser/tree/ArcGISLocationProvider)):
-![sampleapp1](https://cloud.githubusercontent.com/assets/1378165/5062617/3419eef4-6d7b-11e4-8c8b-a6c4eaa212f0.jpg)
-
+# Copyright
+This library uses code from the following works:
+*  [NMEA Parser library](https://github.com/dotMorten/NmeaParser) published under the [MS-PL license](https://opensource.org/licenses/MS-PL).
--- Tools/Nuget/buildNuget.cmd
+++ Tools/Nuget/buildNuget.cmd
@@ -3,28 +3,28 @@
 @echo *******************************************
 @echo * COPYING BINARIES FOR NUGET              *
 @echo *******************************************
-REM msbuild ..\..\src\NmeaParser.sln /t:Rebuild /p:Configuration=Release
-msbuild ..\..\src\NmeaParser.WinDesktop\NmeaParser.WinDesktop.csproj /t:Rebuild /p:Configuration=Release
-msbuild ..\..\src\NmeaParser.WinStore\NmeaParser.WinStore.csproj /t:Rebuild /p:Configuration=Release
-msbuild ..\..\src\NmeaParser.WinPhone\NmeaParser.WinPhone.csproj /t:Rebuild /p:Configuration=Release
-msbuild ..\..\src\NmeaParser.UWP\NmeaParser.UWP.csproj /t:Rebuild /p:Configuration=Release
-msbuild ..\..\src\NmeaParser.Android\NmeaParser.Android.csproj /t:Rebuild /p:Configuration=Release
-msbuild ..\..\src\NmeaParser.iOS\NmeaParser.iOS.csproj /t:Rebuild /p:Configuration=Release
-xcopy ..\..\src\bin\Release\NmeaParser.WinStore.dll .\NmeaParser\lib\netcore45\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.WinStore.xml .\NmeaParser\lib\netcore45\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.WinPhone.dll .\NmeaParser\lib\wpa\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.WinPhone.xml .\NmeaParser\lib\wpa\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.WinDesktop.dll .\NmeaParser\lib\net40-client\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.WinDesktop.xml .\NmeaParser\lib\net40-client\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.UWP.dll .\NmeaParser\lib\uap10.0\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.UWP.xml .\NmeaParser\lib\uap10.0\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.Android.dll .\NmeaParser\lib\MonoAndroid10\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.Android.xml .\NmeaParser\lib\MonoAndroid10\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.iOS.dll .\NmeaParser\lib\Xamarin.iOS10\ /Y
-xcopy ..\..\src\bin\Release\NmeaParser.iOS.xml .\NmeaParser\lib\Xamarin.iOS10\ /Y
+REM msbuild ..\..\src\GrblConnector.sln /t:Rebuild /p:Configuration=Release
+msbuild ..\..\src\GrblConnector.WinDesktop\GrblConnector.WinDesktop.csproj /t:Rebuild /p:Configuration=Release
+msbuild ..\..\src\GrblConnector.WinStore\GrblConnector.WinStore.csproj /t:Rebuild /p:Configuration=Release
+msbuild ..\..\src\GrblConnector.WinPhone\GrblConnector.WinPhone.csproj /t:Rebuild /p:Configuration=Release
+msbuild ..\..\src\GrblConnector.UWP\GrblConnector.UWP.csproj /t:Rebuild /p:Configuration=Release
+msbuild ..\..\src\GrblConnector.Android\GrblConnector.Android.csproj /t:Rebuild /p:Configuration=Release
+msbuild ..\..\src\GrblConnector.iOS\GrblConnector.iOS.csproj /t:Rebuild /p:Configuration=Release
+xcopy ..\..\src\bin\Release\GrblConnector.WinStore.dll .\GrblConnector\lib\netcore45\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.WinStore.xml .\GrblConnector\lib\netcore45\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.WinPhone.dll .\GrblConnector\lib\wpa\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.WinPhone.xml .\GrblConnector\lib\wpa\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.WinDesktop.dll .\GrblConnector\lib\net40-client\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.WinDesktop.xml .\GrblConnector\lib\net40-client\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.UWP.dll .\GrblConnector\lib\uap10.0\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.UWP.xml .\GrblConnector\lib\uap10.0\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.Android.dll .\GrblConnector\lib\MonoAndroid10\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.Android.xml .\GrblConnector\lib\MonoAndroid10\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.iOS.dll .\GrblConnector\lib\Xamarin.iOS10\ /Y
+xcopy ..\..\src\bin\Release\GrblConnector.iOS.xml .\GrblConnector\lib\Xamarin.iOS10\ /Y
 
 
 @echo *******************************************
 @echo * BUILDING NUGET PAKCAGE					*
 @echo *******************************************
-nuget pack NmeaParser\NmeaParser.nuspec -o .\
+nuget pack GrblConnector\GrblConnector.nuspec -o .\
